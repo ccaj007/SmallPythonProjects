7e1f8b4c94f67cdc568d0387f52e3e04683604a9
C L <ccaj007@gmail.com>
2022-06-16 23:04:12 -0400

    update

--- bagels.py
+++ bagels.py
@@ -11,3 +11,36 @@ secret number.
 """
 
 import random
+import sys
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def get_secretNum():
+    """Returns a string number made up of NUM_DIGITS unique random digits"""
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+def find_out(guess):
+    if len(guess) != 3:
+        sys.exit('invalid guess, must be 3 digits')
+    answer = f'{random.randrange(1, 10**3):03}'
+    list_ans = list(answer)
+    list_guess = list(guess)   
+
+    print(list_ans, list_guess)
+    for i in list_guess:
+        if i in list_ans:
+            print('got it')
+guess = input('enter a 3 digit guess: ')
+find_out(guess)
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
